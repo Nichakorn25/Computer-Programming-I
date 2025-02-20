c466f8c3e98d05eeb758e74b4bc4ce94589ff1e8
Nichakorn25 <nichakorn391@gmail.com>
2025-02-20 17:11:38 +0700

    แลปในสไลด์

---
+++ Week6.py
@@ -0,0 +1,51 @@
+#แลปในสไลด์
+
+# 6.1 เขียนโปรแกรมเพื่อรับเลขจ ำนวนเต็มเก็บไว้ที่ตัวแปร n แล้วแสดงออกเป็น
+# เลขฐำนสอง ฐำนแปด และ ฐำนสิบหก ดังตัวอย่ำงกำรรันโปรแกรม
+# ตัวอย่ำงกำรรันโปรแกรม
+# Enter a number: 21
+# Type of n = <class 'int'>
+# Decimal number 21 = Binary number 0b10101
+# Decimal number 21 = Octal number 0o25
+# Decimal number 21 = Hexadecimal number 0x15
+
+# =================================================================================
+
+
+# 6.2 เขียนโปรแกรมเพื่อรับสตริงเก็บไว้ที่ตัวแปร s แล้วแสดงออกที่จอภำพ
+# ในรูปแบบต่ำง ๆ โดยกำรระบุล ำดับที่ ดังตัวอย่ำงกำรรันโปรแกรม
+# ตัวอย่ำงกำรรันโปรแกรม
+# Enter a string: Suranaree
+# You entered "Suranaree"
+# There are 9 characters.
+# The first character is 'S'.
+# The last character is 'e'.
+# The first five-character is 'Suran'.
+# "Suranaree" is reversed to "eeranaruS".
+
+# =================================================================================
+
+
+# 6.3 เขียนโปรแกรมเพื่อรับสตริงเก็บไว้ที่ตัวแปร s แล้วแสดงกำรใช้เมธอดต่ำง ๆ เพื่อแสดงผล
+# ให้ได้ดังตัวอย่ำงกำรรันโปรแกรม
+# ตัวอย่ำงกำรรันโปรแกรม
+# Enter a string: you are smart
+# You entered a string "you are smart".
+# Capital only first character of each word ==> You Are Smart
+# All Uppercase ==> YOU ARE SMART
+# All Lowercase ==> you are smart
+# Replace a space with "---" ==> you---are---smart
+
+# =================================================================================
+
+
+# 6.4 เขียนโปรแกรมเพื่อรับสตริง รับอินเด็กซ์ จุดเริ่มต้น จุดสิ้นสุด ล ำดับขั้น
+# ในกำรเลื่อน แล้วแสดงผลออกที่จอภำพดังตัวอย่ำงกำรรันโปรแกรม
+# ตัวอย่ำงกำรรันโปรแกรม
+# Enter a string: 12345678901234567890
+# Display data at index: -3
+# Data at index -3 is '8'
+# Start display data at index: -2
+# Stop display data at index: -16
+# Step to move: -3
+# Data from index -2 to -16 is '96307'
